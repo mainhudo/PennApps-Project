a775f4a89193bcfd77259eaee4ea2515097563f0
joylees <47282645+joylees@users.noreply.github.com>
2019-02-02 16:23:49 -0500

    Add files via upload
    first upload test

---
+++ testFile.py
@@ -0,0 +1,23 @@
+##
+## Temperature Conversion
+##   Convert Fahrenheit degrees to Celsius based on user input
+
+temperature = float(input())
+print("Enter temperature: ")
+c = (temperature - 32)*5/9
+round(c,1)
+print("The temperature is %.1f" % c )
+if temperature <70:
+   print("Too cold")
+   import datetime as dt
+   print(dt.datetime.now ())
+
+elif temperature >=70 and temperature <80:
+   print("Just right")
+   import datetime as dt
+   print(dt.datetime.now ())
+
+else:
+   print("Too hot")
+   import datetime as dt
+   print(dt.datetime.now ())
